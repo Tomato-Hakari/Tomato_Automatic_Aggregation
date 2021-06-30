5b0a707dada3866e3cfc0b60d7a20379aecdbaee
TeraJun <teraken_terao@icloud.com>
2021-06-30 21:39:29 +0900

    収量測定画面を新規作成

---
+++ Tomato_Automatic_Aggregation/View/InputYieldView.swift
@@ -0,0 +1,43 @@
+//
+//  InputYieldView.swift
+//  Tomato_Automatic_Aggregation
+//
+//  Created by ミニトマト自動集計 on 2021/06/30.
+//
+
+import SwiftUI
+
+struct InputYieldView: View {
+    // シートが開いている状態
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        VStack{
+            Text("開発中").font(.title).padding()
+            HStack(){
+                Text("品種ID：" + Variety.InputVarietyID)
+                Spacer()
+            }
+            .frame(width: 250.0)
+            HStack(){
+                Text("品種名：" + Variety.InputVarietyName)
+                Spacer()
+            }.frame(width: 250.0)
+        }
+        .padding()
+        .navigationBarTitle("収量入力")
+        .toolbar{
+            ToolbarItem(placement: .navigationBarTrailing){
+                Button("閉じる"){
+                    isPresented = false
+                }
+            }
+        }
+    }
+}
+
+struct InputYieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputYieldView(isPresented: Binding.constant(false))
+    }
+}

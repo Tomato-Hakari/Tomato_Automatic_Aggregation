ad0e6f2f84f913a1ed87c7f6945886eaddf59e61
Tomato-Hakari <teraken_terao@icloud.com>
2021-07-15 05:27:56 +0900

    healthplanetアプリへ遷移するボタン・収量データを表示するwebページへ遷移するボタンを追加

--- Tomato_Automatic_Aggregation/View/InputYieldView.swift
+++ Tomato_Automatic_Aggregation/View/InputYieldView.swift
@@ -6,23 +6,57 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct InputYieldView: View {
+    // 体重計から得たデータを格納
+    struct ScaleData: Codable {
+        var date: String
+        var keydata: String
+        var model: String
+        var tag: String
+    }
+    
+    @State var scale: [ScaleData] = []
+    
     // シートが開いている状態
     @Binding var isPresented: Bool
     
+    // HealthPlanetアプリのURL
+    let HealthPlanetURL = URL(string: "healthplanet://")
+    
+    // apiscaleのURL
+    let apiscaleURL = URL(string: "https://apiscale.herokuapp.com")
+    
     var body: some View {
         VStack{
-            Text("開発中").font(.title).padding()
+            Button("Health Planet アプリへ遷移"){
+                UIApplication.shared.open(HealthPlanetURL!, options: [.universalLinksOnly: false], completionHandler: { completed in
+                    print(completed)
+                })
+            }
+            .padding()
             HStack(){
                 Text("品種ID：" + Variety.InputVarietyID)
                 Spacer()
             }
             .frame(width: 250.0)
+            
             HStack(){
                 Text("品種名：" + Variety.InputVarietyScionName)
                 Spacer()
             }.frame(width: 250.0)
+            
+            Button("収量データ取得"){
+                UIApplication.shared.open(apiscaleURL!, options: [.universalLinksOnly: false], completionHandler: { completed in
+                    print(completed)
+                })
+            }
+            .padding()
+            
+            HStack(){
+                Text("収量：")
+            }
         }
         .padding()
         .navigationBarTitle("収量入力")
@@ -41,3 +75,4 @@ struct InputYieldView_Previews: PreviewProvider {
         InputYieldView(isPresented: Binding.constant(false))
     }
 }
+

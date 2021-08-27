b97042c1936060e729b6d18c48f476950077e325
Tomato-Hakari <teraken_terao@icloud.com>
2021-08-27 14:00:59 +0900

    自作APIから得たデータを格納する構造体を追加

---
+++ Tomato_Automatic_Aggregation/Objects/Scale_Data.swift
@@ -0,0 +1,28 @@
+//
+//  Scale_Data.swift
+//  Tomato_Automatic_Aggregation
+//
+//  Created by ミニトマト自動集計 on 2021/08/19.
+//
+
+import Foundation
+
+struct Scale_Data: Decodable, Identifiable {
+    var id: Int
+    var date: String
+    var weight: String
+    var model: String
+    var tag: String
+    var created_at: String
+    var updated_at: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case date = "date"
+        case weight = "keydata"
+        case model = "model"
+        case tag = "tag"
+        case created_at = "created_at"
+        case updated_at = "updated_at"
+    }
+}

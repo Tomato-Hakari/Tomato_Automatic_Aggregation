cd1eac121b8e37521ab1c922ff4961e989f0b750
Tomato-Hakari <teraken_terao@icloud.com>
2021-09-28 04:50:30 +0900

    データベースに送信できたかどうかのフラグを新規作成

---
+++ Tomato_Automatic_Aggregation/Objects/flag.swift
@@ -0,0 +1,13 @@
+//
+//  flag.swift
+//  Tomato_Automatic_Aggregation
+//
+//  Created by ミニトマト自動集計 on 2021/09/28.
+//
+
+import Foundation
+
+class flag: ObservableObject {
+    static var isSuccessed: Bool = false
+}
+

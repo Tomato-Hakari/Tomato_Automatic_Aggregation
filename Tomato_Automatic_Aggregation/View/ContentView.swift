df47bd019abfc42e7785ab0b91f29caa1a59a085
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-21 20:43:10 +0900

    ボタンデザインを変更 データ確認画面へのリンク、設定画面へのリンクを追加

--- Tomato_Automatic_Aggregation/View/ContentView.swift
+++ Tomato_Automatic_Aggregation/View/ContentView.swift
@@ -12,65 +12,101 @@ struct ContentView: View {
     @ObservedObject var reloader = Reloader()
     // データ入力画面が表示されているか
     @State var isShowInputView: Bool = false
+    // データ表示画面が表示されているか
+    @State var isShowCheckView: Bool = false
+    // 設定画面が表示されているか
+    @State var isShowConfigView: Bool = false
     // アラート表示用フラグ
     @State var alertFlag = false
     
     var body: some View {
-        VStack(){
-            ZStack(){
-                HStack(){
-                    Text("\(reloader.value)")
-                    Spacer()
-                    Button("リセット"){
-                        DataManagement.ResetVariety()
-                        alertFlag = true
+        ZStack{
+            if flag.currentmode == "test" {
+                Color.blue.edgesIgnoringSafeArea(.all)
+            }
+            VStack(){
+                ZStack(){
+                    HStack(){
+                        Text("\(reloader.value)")
+                        Spacer()
+                        Button("リセット"){
+                            DataManagement.ResetVariety()
+                            alertFlag = true
+                        }
+                        .alert(isPresented: $alertFlag){
+                            Alert(title: Text("入力された品種データを\nリセットしました"))
+                        }
+                        .padding()
+                        .hidden()
+                        
                     }
-                    .alert(isPresented: $alertFlag){
-                        Alert(title: Text("入力された品種データを\nリセットしました"))
-                    }
-                    .padding()
-                    .hidden()
+                    Text("ミニトマト自動集計").font(.title).fontWeight(.bold).padding()
                     
                 }
-                Text("ミニトマト自動集計").font(.title).fontWeight(.bold).padding()
+                Spacer()
+               
+                if flag.isSuccessed {
+                    Text("データの送信に成功しました!")
+                        .font(.system(size: 30))
+                        .foregroundColor(.orange)
+                        .padding()
+                }
                 
-            }
-            Spacer()
-           
-            if flag.isSuccessed {
-                Text("データの送信に成功しました!")
-                    .font(.system(size: 30))
-                    .foregroundColor(.orange)
-                    .padding()
-            }
-            
-            HStack{
-                Button(action: {}){
-                    VStack{
-                    Text("設定")
-                        .font(.system(size: 50))
-                        .font(.largeTitle)
-                        Text("※開発中")
+                HStack{
+                    Button(action: {
+                        isShowCheckView = true
+                    }) {
+                        Text("データ確認")
+                            .frame(width: 300, height: 300)
+                            .foregroundColor(.black)
+                            .font(.system(size: 50))
+                                            .font(.largeTitle)
                     }
+                    .sheet(isPresented: $isShowCheckView) {
+                        GetCheckDataView(isPresented: self.$isShowCheckView)
+                    }
+                    .background(Color.green)
+                    .padding()
+                    
+                    Button(action: {
+                        flag.isSuccessed = false
+                        isShowInputView = true
+                    }) {
+                        Text("データ入力")
+                            .frame(width: 300, height: 300)
+                            .font(.system(size: 50))
+                                            .font(.largeTitle)
+                                            .foregroundColor(.black)
+                    }
+                    .sheet(isPresented: $isShowInputView){
+                       InputVarietyView(isPresented: self.$isShowInputView)
+                    }
+                    .background(Color.orange)
+                    .padding()
                 }
-                .disabled(true)
-                .padding()
-                Button("データ入力"){
+                
+                Button(action: {
                     flag.isSuccessed = false
-                    isShowInputView = true
+                    isShowConfigView = true
+                }) {
+                    Text("設定")
+                        .frame(width: 150, height: 100)
+                        .font(.system(size: 50))
+                                        .font(.largeTitle)
+                                        .foregroundColor(.black)
                 }
-                .sheet(isPresented: $isShowInputView){
-                   InputVarietyView(isPresented: self.$isShowInputView)
+                .sheet(isPresented: $isShowConfigView){
+                   ConfigView(isPresented: self.$isShowConfigView)
                 }
-                .font(.system(size: 50))
-                .font(.largeTitle)
+                .background(Color.gray)
                 .padding()
+                
+                
+                Spacer()
             }
-            
-            Spacer()
-        }
-        .onAppear {
-            reloader.start()
+            .onAppear {
+                reloader.start()
+            }
         }
     }
 }

630cebab7e2a10e1c3fec5ccb4dafacaf130d8c4
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-21 20:46:37 +0900

    データベースから受け取るデータを格納する構造体を定義

---
+++ Tomato_Automatic_Aggregation/Objects/CheckData.swift
@@ -0,0 +1,21 @@
+//
+//  CheckData.swift
+//  Tomato_Automatic_Aggregation
+//
+//  Created by ミニトマト自動集計 on 2021/10/18.
+//
+
+import Foundation
+
+struct CheckData: Codable {
+    var adddatetime: String
+    var varietyID: String
+    var scion: String?
+    var scionshort: String?
+    var rootstock: String?
+    var issurvey: Bool
+    var remarks: String?
+    var measurementdatetime: String
+    var yield: Float
+
+}

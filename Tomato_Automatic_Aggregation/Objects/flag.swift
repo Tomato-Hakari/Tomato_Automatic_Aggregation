9d6c2038b7009560d5e1783f7e9cfad8330f52a2
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-21 20:47:09 +0900

    動作モードを指定する定数を追加

--- Tomato_Automatic_Aggregation/Objects/flag.swift
+++ Tomato_Automatic_Aggregation/Objects/flag.swift
@@ -9,5 +9,7 @@ import Foundation
 
 class flag: ObservableObject {
     static var isSuccessed: Bool = false
+    static var currentmode: String = "trial"
+    static let modes: [String] = ["trial", "test"]
 }
 

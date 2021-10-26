de8ba059274bff0cb37e70eab6d56befa6515846
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-26 13:49:27 +0900

    表示する期間の変更に対応 複数選択に対応

--- Tomato_Automatic_Aggregation/View/ScaleDataListView.swift
+++ Tomato_Automatic_Aggregation/View/ScaleDataListView.swift
@@ -12,7 +12,9 @@ struct ScaleDataListView: View {
     // シートが開いている状態
     @Binding var isPresented: Bool
     
-    @ObservedObject var reloader = Reloader()
+    let periods = ["24時間","1ヶ月","全期間"]
+    
+    @State var selectedPeriod: Int = 0;
     
     @ObservedObject var period = LoadingPeriod()
     
@@ -20,25 +22,45 @@ struct ScaleDataListView: View {
     
     @ObservedObject var scaledata = ScaleDataFetcher()
     
-    @State var selectedScaleDate:String = ""
-    @State var selectedScaleWeight:String = ""
+    @State var selectedScaleData: [SelectedScaleData] = []
+    
+    @State var hiddendatas: Int = 0;
     
     // Pickerで選択された品種
-    @State var selectedData:Int? = 0
+    @State var selectedNumbers = Set<Int>()
     
     var body: some View {
         VStack{
-            NavigationLink(destination: SendDataToServerView(isPresented: $isPresented, selectedScaleDate: selectedScaleDate, selectedScaleWeight: selectedScaleWeight), isActive: $isButtonActive){
+            NavigationLink(destination: SendDataToServerView(isPresented: $isPresented, selectedScaleData: selectedScaleData), isActive: $isButtonActive){
                 EmptyView()
             }
             ZStack{
-                GroupBox(label: Text("適切な収量を選択")) {
-                    List(selection: $selectedData) {
+                GroupBox(label:VStack{
+                    Text("適切な収量を選択")
+                    HStack{
+                        Text("表示する期間を選択6t6→")
+                        Picker(selection: $selectedPeriod, label: Text(""), content: {
+                            ForEach(0..<periods.count, id: \.self) { value in
+                                Text("\(periods[value])")
+                                    .tag(value)
+                            }
+                        })
+                            .pickerStyle(SegmentedPickerStyle())
+                    }
+                } ) {
+                    List(selection: $selectedNumbers) {
                         ForEach(0..<scaledata.scale_datum.count, id:\.self) { index in
-                            Text("\(DataManagement.ProcessDate(DateString: scaledata.scale_datum[index].date)) \(scaledata.scale_datum[index].weight)kg")
+                            if selectedPeriod == 0 &&  DataManagement.isToday(dateString: scaledata.scale_datum[index].date, format: "yyyyMMddHHmm") {
+                                Text("\(DataManagement.ProcessDate(DateString: scaledata.scale_datum[index].date)) \(scaledata.scale_datum[index].weight)kg")
+                            } else if selectedPeriod == 1 && DataManagement.isTomonth(dateString: scaledata.scale_datum[index].date, format: "yyyyMMddHHmm") {
+                                Text("\(DataManagement.ProcessDate(DateString: scaledata.scale_datum[index].date)) \(scaledata.scale_datum[index].weight)kg")
+                            } else if selectedPeriod == 2 {
+                                Text("\(DataManagement.ProcessDate(DateString: scaledata.scale_datum[index].date)) \(scaledata.scale_datum[index].weight)kg")
+                            }
                         }
                     }.environment(\.editMode, .constant(.active))
                 }
+                
                 if scaledata.scale_datum.count == 0 {
                     Text("読み込み中\(period.period)")
                         .font(.system(size: 50))
@@ -49,8 +71,12 @@ struct ScaleDataListView: View {
                         }
                         .onDisappear{
                             period.stop()
-                            print("scale_data.measurementdatetime:\(scaledata.scale_datum[0].date)")
                         }
+                } else if hiddendatas == scaledata.scale_datum.count {
+                    Text("データがありません")
+                        .font(.system(size: 50))
+                        .multilineTextAlignment(.leading)
+                        .frame(width: 300.0)
                 }
             }
         }
@@ -58,30 +84,26 @@ struct ScaleDataListView: View {
         .onAppear {
             scaledata.deleteAll()
             scaledata.load()
+            hiddendatas = 0
         }
-        .navigationBarTitle("収量選択\(reloader.value)")
+        .navigationBarTitle("収量選択")
         .toolbar{
             ToolbarItem(placement: .navigationBarTrailing){
                 Button("入力データ確認") {
-                    if scaledata.scale_datum[selectedData!].weight != "" {
-                        selectedScaleDate = DataManagement.ProcessDate(DateString: scaledata.scale_datum[selectedData!].date)
-                        selectedScaleWeight = scaledata.scale_datum[selectedData!].weight
+                    let sortedNumbers = selectedNumbers.sorted()
+                    for num in sortedNumbers {
+                        selectedScaleData.append(SelectedScaleData(date: DataManagement.ProcessDate(DateString: scaledata.scale_datum[num].date), weight: scaledata.scale_datum[num].weight))
                     }
-                    if selectedScaleWeight != "" {
+                    if !selectedScaleData.isEmpty {
                         isButtonActive = true
                     }
                 }
             }
         }
         .onAppear {
-            reloader.start()
-            scaledata.load()
-        }
-    }
-}
 
-struct ScaleDataListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ScaleDataListView(isPresented: Binding.constant(false))
+            scaledata.load()
+            selectedScaleData.removeAll()
+        }
     }
 }

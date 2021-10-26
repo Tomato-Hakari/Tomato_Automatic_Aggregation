a38728a712c7ff33bd88607031221fc493afe535
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-26 14:10:20 +0900

    ResetVarietyメソッドを追加

--- Tomato_Automatic_Aggregation/Objects/Variety.swift
+++ Tomato_Automatic_Aggregation/Objects/Variety.swift
@@ -14,5 +14,15 @@ class Variety: ObservableObject {
     static var InputVarietyRootstockName: String = ""
     static var isInputVarietysYieldSurvey: Bool = false
     static var InputVarietyRemarks: String = ""
+    
+    // Varietyクラスの値をリセット
+    class func ResetVariety() {
+        Variety.InputVarietyID = ""
+        Variety.InputVarietyScionName = ""
+        Variety.InputVarietyScionShort = ""
+        Variety.InputVarietyRootstockName = ""
+        Variety.isInputVarietysYieldSurvey = false
+        Variety.InputVarietyRemarks = ""
+    }
 }
 

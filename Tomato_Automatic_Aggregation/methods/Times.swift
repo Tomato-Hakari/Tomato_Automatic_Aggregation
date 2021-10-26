54e22250a6325f555cfe7f7d6d81074688e9db67
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-26 14:17:41 +0900

    コメント文追加

--- Tomato_Automatic_Aggregation/methods/Times.swift
+++ Tomato_Automatic_Aggregation/methods/Times.swift
@@ -8,11 +8,13 @@
 import Foundation
 import SwiftUI
 
+// 空白の長さを0.5秒毎に変えることによる画面再読み込みクラス
 class Reloader: ObservableObject {
     @Published var value: String = ""
     @State private var roop: Int = 0
     private var timer = Timer()
     
+    // 使いたいViewの.onAppearで使用
     func start() {
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _  in
             self.value = ""

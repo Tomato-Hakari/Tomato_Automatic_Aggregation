45c62b728dd6d384ef65a9b7fd22861bda696447
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-26 14:11:04 +0900

    ResetVarietyメソッドの移動に伴う変更

--- Tomato_Automatic_Aggregation/View/ContentView.swift
+++ Tomato_Automatic_Aggregation/View/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                         Text("\(reloader.value)")
                         Spacer()
                         Button("リセット"){
-                            DataManagement.ResetVariety()
+                            Variety.ResetVariety()
                             alertFlag = true
                         }
                         .alert(isPresented: $alertFlag){

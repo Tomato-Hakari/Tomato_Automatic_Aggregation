41272b527d7a04b3d32bbe936502070af1434593
Tomato-Hakari <teraken_terao@icloud.com>
2021-10-29 23:49:08 +0900

    フラグ変数名を変更

--- Tomato_Automatic_Aggregation/Objects/flag.swift
+++ Tomato_Automatic_Aggregation/Objects/flag.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class flag: ObservableObject {
-    static var isSuccessed: Bool = false
+    static var isSuccessedSendings: Bool = false
     static var currentmode: String = "trial"
     static let modes: [String] = ["trial", "test"]
 }

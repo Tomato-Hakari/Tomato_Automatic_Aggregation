36013a2b89a9660c91a4b48e34a760ad783dfcb5
Tomato-Hakari <teraken_terao@icloud.com>
2021-11-01 21:29:14 +0900

    自作APIが利用不可能のときの処理を追加

--- Tomato_Automatic_Aggregation/methods/ScaleDatafetcher.swift
+++ Tomato_Automatic_Aggregation/methods/ScaleDatafetcher.swift
@@ -11,16 +11,22 @@ import Combine
 // heroku上に作成した自作apiから体重計から取得したデータをScale_Data型配列に格納するクラス
 class ScaleDataFetcher: ObservableObject {
     @Published var scale_datum: [Scale_Data] = []
+    @Published var isHerokuDown: Bool = false
 
     init() {
         load()
     }
 
     func load() {
+        self.isHerokuDown = false
         let url = URL(string: "https://scaleapi.herokuapp.com/scale_data")!
         URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                self.scale_datum = try! JSONDecoder().decode([Scale_Data].self, from: data!)
+            if let data = data {
+                DispatchQueue.main.async {
+                    self.scale_datum = try! JSONDecoder().decode([Scale_Data].self, from: data)
+                }
+            } else {
+                self.isHerokuDown = true
             }
         }.resume()
     }

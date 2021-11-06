935125c6b9de72cdc03ea49a3e1053e19ca2a428
Tomato-Hakari <teraken_terao@icloud.com>
2021-11-06 20:05:44 +0900

    テストモードへの移行に管理者ログインが必要なように変更

--- Tomato_Automatic_Aggregation/View/ConfigView.swift
+++ Tomato_Automatic_Aggregation/View/ConfigView.swift
@@ -7,11 +7,27 @@
 
 import SwiftUI
 
+struct DeveloperLogin {
+    var id = ""
+    var password = ""
+}
+struct DeveloperDB: Codable {
+    let id: String
+    let pass: Int
+}
+
 struct ConfigView: View {
     // シートが開いている状態
     @Binding var isPresented: Bool
     
     @State var selectedMode: Int? = 0
+    
+    @State var toDeveloperLogin: Bool = false
+    
+    @State var isLoginError: Bool = false
+    
+    @State var login = DeveloperLogin()
+    let Developer: DeveloperDB = Bundle.main.decodeJSON(file: "Developer.json")
     
     var body: some View {
         NavigationView{
@@ -24,12 +40,16 @@ struct ConfigView: View {
                     }
                     .environment(\.editMode, .constant(.active))
                     .onChange(of: selectedMode){ [selectedMode] newvalue in
+                        if newvalue == 1 {
+                            toDeveloperLogin = true
+                        }
                         if (newvalue != nil) {
                             flag.currentmode = flag.modes[newvalue!]
                             flag.currentmodeNum = newvalue!
                         } else {
                             self.selectedMode = selectedMode
                         }
+                        
                     }
                 }
             }
@@ -46,6 +66,36 @@ struct ConfigView: View {
             .onAppear {
                 self.selectedMode = flag.currentmodeNum
             }
+        }
+        .textFieldAlert(
+            isShowing: $toDeveloperLogin,
+            title: "開発者用ログイン",
+            text1: $login.id,
+            text1Placeholder: "開発者ID",
+            isSecureFieldInText1: false,
+            text2: $login.password,
+            text2Placeholder: "パスワード",
+            isSecureFieldInText2: true,
+            leftButtonTitle: "キャンセル",
+            leftButtonAction: {
+                self.selectedMode = 0
+                UIApplication.shared.endEditing()
+            },
+            rightButtonTitle: "ログイン",
+            rightButtonAction: {
+                UIApplication.shared.endEditing()
+                var hash = 0
+                for code in login.password.utf8 {
+                    hash += Int(code)
+                }
+                if (login.id != Developer.id) || (hash != Developer.pass) {
+                    self.selectedMode = 0
+                    login.password = ""
+                    isLoginError = true
+                }
+            })
+        .alert(isPresented: $isLoginError) {
+            Alert(title: Text("ログイン失敗"), message: Text("ログインIDまたはパスワードが異なります。"), dismissButton: .default(Text("OK")))
         }
     }
 }

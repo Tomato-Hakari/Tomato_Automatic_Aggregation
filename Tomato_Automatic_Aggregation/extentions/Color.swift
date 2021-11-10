1f246a4d25a92a9c3db333ec5d4a19b3e6e3ca15
Tomato-Hakari <teraken_terao@icloud.com>
2021-11-10 21:42:10 +0900

    Colorクラスに独自で色を追加するファイル追加

---
+++ Tomato_Automatic_Aggregation/extentions/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  Tomato_Automatic_Aggregation
+//
+//  Created by ミニトマト自動集計 on 2021/11/07.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static var lightgray: Color {
+        return Color(red: 204/255, green: 204/255, blue: 204/255, opacity: 1.0)
+    }
+}

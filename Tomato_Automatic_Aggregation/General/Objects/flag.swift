a54286aacaca72c4bf9a917f815c9bf75c82d9ec
Tomato-Hakari <teraken_terao@icloud.com>
2022-03-16 10:06:47 +0900

    コメント文を追加

--- Tomato_Automatic_Aggregation/General/Objects/flag.swift
+++ Tomato_Automatic_Aggregation/General/Objects/flag.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 class flag: ObservableObject {
+    // データベースにデータが送信できたかどうか(ほぼ意味ない)
     static var isSuccessedSendings: Bool = false
+    
+    // 現在のモード(trial or test)
     static var currentmode: String = "trial"
+    
+    // 現在のモード(trial:0, test:1)
+    // currentmodeと状態が異なることがないよう注意
     static var currentmodeNum: Int = 0
+    
+    // モードの種類(currentmodeとcurrentmodeNumの対応はこれで決まる)
     static let modes: [String] = ["trial", "test"]
 }
